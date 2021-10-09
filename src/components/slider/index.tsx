fffeb9d12c55cd8e416ec3ab9269679903623094
joaocdfarias <joaocdfariass@gmail.com>
2021-10-09 17:03:22 -0300

    slider item selected

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
-// import { imgMock } from './mock'
+import { imgMock } from './mock'
 import './style.scss'
 
 const Slider = () => {
+  const [product, setProduct] = React.useState(0)
+
+  function changeProduct(index: any) {
+    setProduct(index)
+  }
+
   return (
     <div className="imgWrapper">
-      {/* {imgMock.map((data) => {
+      {imgMock.map((data, index) => {
         return (
           <img
+            onClick={() => changeProduct(index)}
             src={data.imgThumb}
             alt={'Imagem do produto ' + data.id}
             key={data.id}
+            className={product === index ? 'selected' : ''}
           />
         )
-      })} */}
+      })}
     </div>
   )
 }
