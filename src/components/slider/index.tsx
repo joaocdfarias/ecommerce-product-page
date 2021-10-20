7799cc931ae1d1dfa97315a342e552a7ce5d2b17
joaocdfarias <joaocdfariass@gmail.com>
2021-10-19 21:12:40 -0300

    feat: lightbox funcionando

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { imgMock } from './mock'
+
+import closeIcon from '../../assets/icon-close.svg'
+
 import './style.scss'
 
 const Slider = () => {
   const [product, setProduct] = React.useState(0)
+  const [lightboxProduct, setLightboxProduct] = React.useState(0)
+  const [lightbox, setLightbox] = React.useState(false)
 
   function changeProduct(index: any) {
     setProduct(index)
+  }
+
+  function changeLightboxProduct(index: any) {
+    setLightboxProduct(index)
   }
 
   return (
@@ -21,6 +30,7 @@ const Slider = () => {
                 key={data.id}
                 draggable="false"
                 className={product === index ? 'selected' : 'hide'}
+                onClick={() => setLightbox(!lightbox)}
               />
             )
           })}
@@ -39,7 +49,41 @@ const Slider = () => {
             )
           })}
         </div>
+
       </div>
+      {lightbox ? <div className="backdrop">
+        <div className="imgWrapper lightboxWrapper">
+          <div className="selectedImage lightboxImg">
+          <img onClick={() => setLightbox(!lightbox)} className="closeIcon" src={closeIcon} alt="" />
+          {imgMock.map((data, index) => {
+            return (
+              
+              <img
+                src={data.imgSrc}
+                alt={'Imagem do produto ' + data.id}
+                key={data.id}
+                draggable="false"
+                className={lightboxProduct === index ? 'selected' : 'hide'}
+              />
+            )
+          })}
+          </div>
+          <div className="imgSelect lighboxImgSelect">
+          {imgMock.map((data, index) => {
+            return (
+              <img
+                onClick={() => changeLightboxProduct(index)}
+                src={data.imgThumb}
+                alt={'Imagem do produto ' + data.id}
+                key={data.id}
+                draggable="false"
+                className={lightboxProduct === index ? 'selected' : ''}
+              />
+            )
+          })}
+        </div>
+</div>
+      </div> : ''}
     </>
   )
 }
